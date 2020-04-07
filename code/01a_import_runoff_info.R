5cc4529324f008510840a7a4d19554c0e51d787c
Ehsan Ebrahimian <57704200+e-ebrahimian@users.noreply.github.com>
2020-04-07 09:57:16 +0200

    Add files via upload

---
+++ code/01a_import_runoff_info.R
@@ -0,0 +1,20 @@
+list.files('data/raw')
+
+library(data.table)
+
+runoff_stations <- fread('./data/raw/runoff_stations.csv')
+
+head(runoff_stations)
+
+str(runoff_stations)
+
+runoff_stations$station
+
+runoff_stations[, sname := factor(abbreviate(station))]
+runoff_stations[, id := factor(id)]
+runoff_stations[, lat := round(lat, 3)]
+runoff_stations[, lon := round(lon, 3)]
+runoff_stations[, altitude := round(altitude, 0)]
+
+saveRDS(runoff_stations, './data/runoff_stations.rds')
+
